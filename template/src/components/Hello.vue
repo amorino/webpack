0eb096f602aa8227b59ae2ebe5b3c0040825f90e
Erick Bazán <bazan.erick@gmail.com>
2016-10-12 12:06:56 -0500

    Add stylus

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -15,8 +15,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
-}
-</style>
+<style lang="stylus" src="../styles/components/Hello.styl" scoped></style>
